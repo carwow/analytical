8ee9728595a4ad8f77837033dd5de17fdedce00a
Isaac Seymour <i.seymour@oxon.org>
2016-11-08 10:55:52 +0000

    Limit the size of usage of the session store to ~1KB
    Very approximate, but bounds the cookie usage to leave about 75% for
    other things.

--- lib/analytical/session_command_store.rb
+++ lib/analytical/session_command_store.rb
@@ -6,18 +6,21 @@ module Analytical
       @session = session
       @module_key = module_key
       @session_key = ('analytical_'+module_key.to_s).to_sym
-      ensure_session_setup!(initial_list)
+      assign(initial_list || [])
     end
 
     def assign(v)
       self.commands = v
+      trim_commands!
     end
 
     def commands
+      trim_commands!
       @session[@session_key]
     end
     def commands=(v)
       @session[@session_key] = v
+      trim_commands!
     end
 
     def flush
@@ -30,13 +33,17 @@ module Analytical
 
     # Pass any array methods on to the internal array
     def method_missing(method, *args, &block)
-      commands.send(method, *args, &block)
+      commands.send(method, *args, &block).tap { trim_commands! }
     end
 
     private
 
-    def ensure_session_setup!(initial_list=nil)
-      self.commands ||= (initial_list || [])
+    # Make sure to not exceed ~1KB of session storage use (max cookie is 4KB)
+    def trim_commands!
+      serialized_length = @session[@session_key].inspect.length
+      return unless serialized_length > 1000
+      @session[@session_key] = @session[@session_key][0..-2]
+      trim_commands!
     end
 
   end
